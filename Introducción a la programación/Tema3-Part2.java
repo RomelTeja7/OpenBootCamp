ab6428e9604b21b7b2818ae4280b40255c79bd54
RomelTeja7 <118077551+RomelTeja7@users.noreply.github.com>
2022-11-22 16:53:31 -0600

    Update Tema3-Part2.java

--- Introducción a la programación/Tema3-Part2.java
+++ Introducción a la programación/Tema3-Part2.java
@@ -1,1 +1,19 @@
+/*Crear una clase coche.
+Dentro de la clase coche, una variable numérica que almacene el número de puertas que tiene.
+Una función que incremente el número de puertas que tiene el coche.
+Crear un objeto miCoche en el main y añadirle una puerta.
+Mostrar el número de puertas que tiene el objeto.*/
 
+public class Cars {
+    public static void main(String[] args) {
+        coche miCoche = new coche();
+        miCoche.puerta();
+        System.out.println(miCoche.puertas);
+    }
+}
+class coche {
+    int puertas = 4;
+    public void puerta() {
+        this.puertas++;
+    }
+}
