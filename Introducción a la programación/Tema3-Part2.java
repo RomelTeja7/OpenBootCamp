fd8ca4810106e0cc6680ec39d6cc798b1ea58347
RomelTeja7 <118077551+RomelTeja7@users.noreply.github.com>
2022-11-22 16:53:46 -0600

    Update Tema3-Part2.java

--- Introducción a la programación/Tema3-Part2.java
+++ Introducción a la programación/Tema3-Part2.java
@@ -3,7 +3,9 @@ Dentro de la clase coche, una variable numérica que almacene el número de puer
 Una función que incremente el número de puertas que tiene el coche.
 Crear un objeto miCoche en el main y añadirle una puerta.
 Mostrar el número de puertas que tiene el objeto.*/
-
+/*
+ * @author Romel
+ */
 public class Cars {
     public static void main(String[] args) {
         coche miCoche = new coche();
