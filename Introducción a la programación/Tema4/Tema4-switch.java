f208491663399152adae62e1438c6dec3aa059a7
RomelTeja7 <118077551+RomelTeja7@users.noreply.github.com>
2022-11-22 18:28:44 -0600

    Create Tema4-switch.java

---
+++ Introducción a la programación/Tema4/Tema4-switch.java
@@ -0,0 +1,22 @@
+//Switch
+    public static void main(String[] args) {
+
+        String estacion = "a";
+        switch (estacion) {
+            case "Primavera":
+                System.out.println("la estacion es "+ estacion);
+                break;
+            case "Verano":
+                System.out.println("la estacion es "+ estacion);
+                break;
+            case "Otono":
+                System.out.println("la estacion es "+ estacion);
+                break;
+            case "Invierno":
+                System.out.println("la estacion es "+ estacion);
+                break;
+            default:
+                System.out.println("No es una estacion");
+                break;
+        }
+    }
