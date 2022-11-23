86d65b8d12d12fb414cae065ab1eb71d98d2ac5e
RomelTeja7 <118077551+RomelTeja7@users.noreply.github.com>
2022-11-22 18:07:13 -0600

    Update and rename Tema4.java to Tema4-if.java

---
+++ Introducción a la programación/Tema4/Tema4-if.java
@@ -0,0 +1,10 @@
+//IF para numero confirmar si el numero es negativo y positivo 
+
+public static void main(String[] args) {
+        int num = -1;
+        if(num < 0){
+        System.out.println("El numero "+num+" es negativo");
+        }else if(num > 0){
+        System.out.println("El numero "+num+" es positivo");   
+        }
+    }
