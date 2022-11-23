ca1663f3872d326a8174e0b3e3a939b9d37f4557
RomelTeja7 <118077551+RomelTeja7@users.noreply.github.com>
2022-11-22 18:18:02 -0600

    Create Tema4-dowhile.java

---
+++ Introducción a la programación/Tema4/Tema4-dowhile.java
@@ -0,0 +1,9 @@
+//Do While
+    public static void main(String[] args) {
+        int num = 0;
+        do {
+            System.out.println(num);
+            num++;
+        } 
+        while(num < 3);
+    }
