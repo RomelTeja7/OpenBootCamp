7a45a3069023aed803e9708ae3019629592bc393
Romel Tejada <118077551+RomelTeja7@users.noreply.github.com>
2022-12-08 20:44:11 -0600

    Create Tema1-dataType.java

---
+++ Java Basico/Tema1/Tema1-dataType.java
@@ -0,0 +1,13 @@
+public class theme1Exercise {
+
+    public static void main(String[] args) {
+        
+        int years = 20;
+        long month = 8;
+        double height = 1.81;
+        boolean really = true;
+        String name = "Romel Tejada";
+        System.out.println("Hola soy "+name+" tengo "+years+" años, soy del mes "+month+", y mido "+height+
+                "\n tambien es "+really+" que soy de  El Salvador");
+    }
+}
