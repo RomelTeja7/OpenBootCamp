3cf322f2ee20ad7a3bb368d54ef8fc014cc97088
Romel Tejada <118077551+RomelTeja7@users.noreply.github.com>
2022-12-12 21:23:25 -0600

    Create tema3-loopConcatenacion.java

---
+++ Java Basico/Tema 3/tema3-loopConcatenacion.java
@@ -0,0 +1,10 @@
+public class theme3Exercise {
+    
+    public static void main(String[] args) {
+        String[] nombres = {"Romel", "Ernesto", "Tejada", "Pena"};
+        for (int i = 0; i < nombres.length; i++) {
+            System.out.print(nombres[i]+" ");
+        }
+        
+    }
+}
