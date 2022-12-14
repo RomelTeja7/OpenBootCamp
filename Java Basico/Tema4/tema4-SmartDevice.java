ba382f47439866c733dbeb1626db3a076773d685
Romel Tejada <118077551+RomelTeja7@users.noreply.github.com>
2022-12-13 21:52:03 -0600

    Update tema4-SmartDevice.java

--- Java Basico/Tema4/tema4-SmartDevice.java
+++ Java Basico/Tema4/tema4-SmartDevice.java
@@ -1,72 +1,74 @@
 public class SmartDevice {
+        String modelo = "";
+        String marca = "";
+        String color = "";
+        Double precio ;
+        String sistema = "";
 
+    public SmartDevice() {
+    }
+        
+    public SmartDevice(String modelo, String marca, String color, Double precio, String sistema){
+        this.modelo = modelo;
+        this.marca = marca;
+        this.color = color;
+        this.precio = precio;
+        this.sistema =  sistema;
+    }
+        
     public static void main(String[] args) {
-
         SmartWatch watch2 = new SmartWatch("Apple Watch", "Apple", "Negro", 699.95, "IOS", "Cuero negro", "cardiaco, pasos");
         SmartPhone phone2 = new SmartPhone("Iphone 14", "Apple", "Blanco", 1199.99, "IOS", 7.0, "40px", "128GB");
+        
         System.out.println(watch2);
         System.out.println(phone2);
+
     }
 
-    static class SmartWatch {
+    static class SmartWatch extends SmartDevice{
 
-        private String modelo;
-        private String marca;
-        private String color;
-        private Double precio;
-        private String sistema;
-        private String sensores;
-        private String correaMaterial;
+        String sensores = "";
+        String correaMaterial = "";
+        
+        public SmartWatch() {
+            
+        }
 
         @Override
         public String toString() {
-            return "SmartWatch{" + "modelo=" + modelo + ", marca=" + marca + ", color=" + color + ", precio=" + precio + ", sistema=" + sistema + ", sensores=" + sensores + ", correaMaterial=" + correaMaterial + '}';
+            return "SmartWatch{" + "sensores=" + sensores + ", correaMaterial=" + correaMaterial + '}';
         }
 
-        public SmartWatch() {
-        }
-
-        public SmartWatch(String modelo, String marca, String color, Double precio, String sistema, String sensores, String correaMaterial) {
-            this.modelo = modelo;
-            this.marca = marca;
-            this.color = color;
-            this.precio = precio;
-            this.sistema = sistema;
+        public SmartWatch(String modelo, String marca, String color, Double precio, String sistema, String sensores, String correaMaterial){
+            super(modelo, marca, color, precio, sistema);
             this.sensores = sensores;
             this.correaMaterial = correaMaterial;
         }
+        
 
     }
 
-    static class SmartPhone {
+    static class SmartPhone extends SmartDevice{
 
-        private String modelo;
-        private String marca;
-        private String color;
-        private Double precio;
-        private String sistema;
-        private Double tamano;
-        private String camara;
-        private String espacio;
+        Double tamano;
+        String camara = "";;
+
+        @Override
+        public String toString() {
+            return "SmartPhone{" + "tamano=" + tamano + ", camara=" + camara + ", espacio=" + espacio + '}';
+        }
+        String espacio = "";;
 
         public SmartPhone() {
+            
         }
 
         public SmartPhone(String modelo, String marca, String color, Double precio, String sistema, Double tamano, String camara, String espacio) {
-            this.modelo = modelo;
-            this.marca = marca;
-            this.color = color;
-            this.precio = precio;
-            this.sistema = sistema;
+            super(modelo, marca, color, precio, sistema);
             this.tamano = tamano;
             this.camara = camara;
             this.espacio = espacio;
         }
-
-        @Override
-        public String toString() {
-            return "SmartPhone{" + "modelo=" + modelo + ", marca=" + marca + ", color=" + color + ", precio=" + precio + ", sistema=" + sistema + ", tamano=" + tamano + ", camara=" + camara + ", espacio=" + espacio + '}';
-        }
-
+        
     }
 }
